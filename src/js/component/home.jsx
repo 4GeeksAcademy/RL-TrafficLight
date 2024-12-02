76dff3d540703cc2544b71fe6c6998ef1b383faa
Rodrigo Lau Ramos <darkbass16@gmail.com>
2024-12-02 20:02:40 +0000

    RL-trafic light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,46 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+
+	const [active, setActive] = useState("red");
+
+	const swithcLight = () => {
+		setActive((prevLight) => {
+			if (prevLight === "red") return "yellow";
+			if (prevLight === "yellow") return "green";
+			return "red";
+		});
+	};
+
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center m-5"><b>Trafic Light</b></h1>
+			<div className="stick"></div>
+			<div className="trafic-light">
+				<div
+					onClick={() => setActive("red")}
+					className="light red"
+					style={{
+						backgroundColor: active === "red" ? "red" : "grey",
+					}}></div>
+				<div
+					onClick={() => setActive("yellow")}
+					className="light yellow"
+					style={{
+						backgroundColor: active === "yellow" ? "yellow" : "grey",
+					}}></div>
+				<div
+					onClick={() => setActive("green")}
+					className="light green"
+					style={{
+						backgroundColor: active === "green" ? "green" : "grey",
+					}}></div>
+			</div>
+			<button onClick={swithcLight} className="buttom btn btn-dark text-light">
+				Cambiar Luz
+			</button>
 		</div>
 	);
 };
